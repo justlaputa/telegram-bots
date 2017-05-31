4929e0e906fe10193ecd7cc40d598742f6beb0c9
Xiao Han <justlaputa@gmail.com>
2017-06-01 08:32:19 +0900

    remove token from build script

--- cats/build_and_push.sh
+++ cats/build_and_push.sh
@@ -4,4 +4,4 @@ env GOOS=linux GOARCH=amd64 go build
 ssh do pkill cats
 scp cats do:~/bots
 
-ssh do "PORT=8100 BOT_TOKEN="330967490:AAGWNslXEpZ9Lj9kWQ-IEGQg7IjOKMQYZSE" nohup ~/bots/cats >> ~/bots/cats.log 2>&1 &"
+ssh do "PORT=8100 BOT_TOKEN=$BOT_TOKEN nohup ~/bots/cats >> ~/bots/cats.log 2>&1 &"
